5deeaf914d226b65404978d1b964621ffb212756
Abhishek jha <abhishekjhagzb2001@gmail.com>
2023-07-27 16:27:49 +0530

    Update loopsProblems.py

--- loopsProblems.py
+++ loopsProblems.py
@@ -15,3 +15,72 @@ navin
 
 prob. 2 
 
+// 
+
+for i in range(10):
+    print(i)
+    
+output 
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+prob. 3
+
+for i in range(11,21,2):
+    print(i)
+    
+output 
+
+11
+13
+15
+17
+19
+
+prob. 4 
+
+for i in range(20,11,-1):
+    print(i)
+
+output 
+20
+19
+18
+17
+16
+15
+14
+13
+12
+
+prob. 5 
+
+av=10
+x=int(input("How many candies you want?"))
+
+i=1
+
+while i<=x:
+    print("candy")
+    i+=1
+
+output 
+How many candies you want? 4
+candy 
+candy 
+candy
+candy
+
+
+prob. 6 
+
+
+
